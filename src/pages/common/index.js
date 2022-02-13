7080c66cc6d3a88acf83b352d23e079e65f600bb
MiguelRetroz <contato.miguelretroz@gmail.com>
2022-02-13 04:16:43 -0300

    feat: add login page in index

--- src/pages/common/index.js
+++ src/pages/common/index.js
@@ -1,7 +1,10 @@
 import Register from './Register';
+import Login from './Login';
 
 export default {
   Register,
+  Login,
 };
 
 export { default as Register } from './Register';
+export { default as Login } from './Login';
