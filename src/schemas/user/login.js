eede94c8fc9ca111797cc32061520122309e88d9
MiguelRetroz <contato.miguelretroz@gmail.com>
2022-02-13 01:54:36 -0300

    feat: user login schema

---
+++ src/schemas/user/login.js
@@ -0,0 +1,15 @@
+import { object, string } from 'yup';
+
+import { regex } from '../../helpers';
+import defaultMessages from './defaultMessages';
+
+const schema = object({
+  email: string(defaultMessages.REQUIRED('Email', 'string'))
+    .required(defaultMessages.REQUIRED('Email'))
+    .matches(regex.email, defaultMessages.INVALID_PATTERN('Email')),
+  password: string(defaultMessages.REQUIRED('Senha'))
+    .required(defaultMessages.REQUIRED('Senha'))
+    .matches(regex.password, defaultMessages.INVALID_PATTERN('Senha')),
+}).required();
+
+export default schema;
