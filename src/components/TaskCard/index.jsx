1990096cfd52d953991fb59fc025e47e94f05eb2
MiguelRetroz <contato.miguelretroz@gmail.com>
2022-02-15 20:06:27 -0300

    feat: add buttons to the `task card`

--- src/components/TaskCard/index.jsx
+++ src/components/TaskCard/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { string } from 'prop-types';
 import dayjs from 'dayjs';
+import { FiEdit } from 'react-icons/fi';
+import { CgCloseR } from 'react-icons/cg';
 
 import BtnStatusOpen from './btn-status/btn-status-open.svg';
 
@@ -14,6 +16,12 @@ function TaskCard({ title, status, createdAt }) {
       <span>{ title }</span>
       <button type="button">
         <img src={ BtnStatusOpen } alt="button change status" />
+      </button>
+      <button type="button">
+        <CgCloseR />
+      </button>
+      <button type="button">
+        <FiEdit />
       </button>
     </CardContainer>
   );
