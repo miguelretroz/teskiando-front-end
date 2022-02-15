69f9fc703b21cf3a7ea7ceb5d8be81fca71bb32d
MiguelRetroz <contato.miguelretroz@gmail.com>
2022-02-15 20:17:06 -0300

    feat: rendering the tasks

--- src/pages/user/Tasks/index.jsx
+++ src/pages/user/Tasks/index.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 
-import { Input } from '../../../components';
+import { Input, TaskCard } from '../../../components';
 import { taskSchemas } from '../../../schemas';
 import { api } from '../../../services';
 
@@ -14,30 +14,46 @@ function Tasks() {
     reset,
   } = useForm({ resolver: yupResolver(taskSchemas.create) });
 
+  const [tasksList, setTasksList] = useState([]);
+
   const onSubmit = async ({ task }) => {
     await api.tasks.register(
       { title: task },
-      (taskCreated) => console.log(taskCreated),
+      (taskCreated) => setTasksList([...tasksList, taskCreated]),
     );
     reset({ task: '' });
   };
 
+  useEffect(() => {
+    const token = JSON.parse(localStorage.getItem('accessToken'));
+    api.tasks.list(setTasksList, token);
+  }, []);
+
   return (
-    <header>
-      <h1>User Name</h1>
-      <form onSubmit={ handleSubmit(onSubmit) }>
-        <Input
-          placeholder="Adicionar nova tarefa"
-          name="task"
-          type="text"
-          maxLength="51"
-          register={ register }
-          displayWarning={ errors.task }
-          warningMessage={ errors.task?.message }
-        />
-        <button type="submit">+</button>
-      </form>
-    </header>
+    <>
+      <header>
+        <h1>User Name</h1>
+        <form onSubmit={ handleSubmit(onSubmit) }>
+          <Input
+            placeholder="Adicionar nova tarefa"
+            name="task"
+            type="text"
+            maxLength="51"
+            register={ register }
+            displayWarning={ errors.task }
+            warningMessage={ errors.task?.message }
+          />
+          <button type="submit">+</button>
+        </form>
+      </header>
+      <main>
+        {
+          tasksList.map(({ _id, ...taskWithoutId }) => (
+            <TaskCard key={ _id } { ...taskWithoutId } />
+          ))
+        }
+      </main>
+    </>
   );
 }
 
