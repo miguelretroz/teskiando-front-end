b34417624fabc82b223f15a5f77b57dc5a855215
MiguelRetroz <contato.miguelretroz@gmail.com>
2022-02-16 20:09:28 -0300

    feat: component `Button`

---
+++ src/components/Button/style.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background-color: ${({ bgColor }) => bgColor || '#3051ff'};
+  border: none;
+  border-radius: 8px;
+  box-shadow: 0 4px 6px ${({ shadowColor }) => shadowColor || '#89a3ff'};
+  color: ${({ color }) => color || 'white'};
+  font-size: 25px;
+  height: ${({ height }) => height || '30px'};
+  line-height: ${({ height }) => height || '30px'};
+  padding: 0;
+  position: relative;
+  transition-duration: 350ms;
+  width: ${({ width }) => width || '150px'};
+
+  :hover {
+    box-shadow: none;
+    top: 1px;
+  }
+`;
+
+export default Button;
--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,13 @@
 import Input from './Input';
 import TaskCard from './TaskCard';
+import Button from './Button';
 
 export default {
   Input,
   TaskCard,
+  Button,
 };
 
 export { default as Input } from './Input';
 export { default as TaskCard } from './TaskCard';
+export { default as Button } from './Button';
