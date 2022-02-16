f7228ec72fe95fcaaac7134ddfbe19a2f3b1015b
MiguelRetroz <contato.miguelretroz@gmail.com>
2022-02-15 22:16:10 -0300

    feat: add missing status button icons

--- src/components/TaskCard/index.jsx
+++ src/components/TaskCard/index.jsx
@@ -5,17 +5,29 @@ import { FiEdit } from 'react-icons/fi';
 import { CgCloseR } from 'react-icons/cg';
 
 import BtnStatusOpen from './btn-status/btn-status-open.svg';
+import BtnStatusInProgress from './btn-status/btn-status-in-progress.svg';
+import BtnStatusFinished from './btn-status/btn-status-finished.svg';
 
 import CardContainer from './style';
 
 function TaskCard({ title, status, createdAt }) {
+  const btnStatusImage = () => {
+    if (status === 'Em progresso') {
+      return BtnStatusInProgress;
+    }
+    if (status === 'Concluído') {
+      return BtnStatusFinished;
+    }
+    return BtnStatusOpen;
+  };
+
   return (
     <CardContainer>
       <span>{ dayjs(createdAt).format('DD/MM/YY HH:MM') }</span>
       <span>{ status }</span>
       <span>{ title }</span>
       <button type="button">
-        <img src={ BtnStatusOpen } alt="button change status" />
+        <img src={ btnStatusImage() } alt="button change status" />
       </button>
       <button type="button">
         <CgCloseR />
