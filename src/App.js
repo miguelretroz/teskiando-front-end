0f26781b8bbc9f3e4458efaf06107e87acad2224
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-03-02 21:23:48 -0300

    feat: implement react query provider

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import Router from './routes';
 
 import './styles/generic/font.css';
 
 import { generic } from './styles';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={ queryClient }>
       <generic.Reset />
       <Router />
-    </>
+    </QueryClientProvider>
   );
 }
 
