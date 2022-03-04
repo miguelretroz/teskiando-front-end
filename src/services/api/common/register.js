007f849c7678db66eddebf5cc1b2dae9c17f5a40
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-03-04 12:12:31 -0300

    refactor: register service

--- src/services/api/common/register.js
+++ src/services/api/common/register.js
@@ -1,24 +1,26 @@
 import { USER_REGISTER } from '../endpoints';
 import { fetchWithBody } from '../fetchs';
 
-export default async (
-  body,
-  setError,
-  redirect,
-  // setIsLoading,
-) => {
-  try {
-    await fetchWithBody('post', USER_REGISTER, body);
-    if (redirect) redirect();
-  } catch ({ response }) {
-    if (response.data) {
-      const { message } = response.data;
-      let key = '';
-      if (message.includes('Nome')) key = 'name';
-      else if (message.includes('Email')) key = 'email';
-      else if (message.includes('Senha')) key = 'password';
-      setError(key, { type: 'api', message });
-    }
-    // setIsLoading(false);
+export const mutationFn = (
+  { name, email, password },
+) => fetchWithBody('post', USER_REGISTER, { name, email, password });
+
+export const onSuccess = (_, { redirect }) => {
+  if (redirect) redirect();
+};
+
+export const onError = (error, { setError }) => {
+  if (error.response) {
+    const { message } = error.response.data;
+    let key = '';
+    if (message.includes('Nome')) key = 'name';
+    else if (message.includes('Email')) key = 'email';
+    else if (message.includes('Senha')) key = 'password';
+    setError(key, { type: 'api', message });
   }
+};
+export default {
+  mutationFn,
+  onSuccess,
+  onError,
 };
