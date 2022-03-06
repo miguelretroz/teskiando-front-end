a36c2e67635cad0cec3f5571a72d5845dbbced5c
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-03-06 05:05:26 -0300

    feat: logo and message loading animation files

---
+++ src/animations/index.js
@@ -0,0 +1,7 @@
+import lottieFiles from './lottieFiles';
+
+export default {
+  lottieFiles,
+};
+
+export { default as lottieFiles } from './lottieFiles';
---
+++ src/animations/lottieFiles/index.js
@@ -0,0 +1,10 @@
+import teskiandoLoadingLogo from './teskiando-loading-logo.json';
+import teskiandoLoadingMessage from './teskiando-loading-message.json';
+
+export default {
+  teskiandoLoadingLogo,
+  teskiandoLoadingMessage,
+};
+
+export { default as teskiandoLoadingLogo } from './teskiando-loading-logo.json';
+export { default as teskiandoLoadingMessage } from './teskiando-loading-message.json';
