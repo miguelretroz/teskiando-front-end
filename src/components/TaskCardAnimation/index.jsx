2b17863fd4ca56a9313c2feb0bbb5c7e46ccbed3
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-03-07 14:41:08 -0300

    refactor: taskCardAnimation remove lottie and use CSS

--- src/components/TaskCardAnimation/index.jsx
+++ src/components/TaskCardAnimation/index.jsx
@@ -1,67 +1,24 @@
-import React, { useEffect, useRef } from 'react';
-import lottie from 'lottie-web';
+import React from 'react';
+import { string } from 'prop-types';
+import dayjs from 'dayjs';
 
-import { lottieFiles } from 'animations';
+import CardContainer from './style';
 
-import PageGlobalStyle, { MainContainer, AnimationContainer } from './style';
-
-function TaskCardAnimation() {
-  const animationContainerOne = useRef(null);
-  const animationContainerTwo = useRef(null);
-  const animationContainerThree = useRef(null);
-  const animationContainerFour = useRef(null);
-  const animationContainerFive = useRef(null);
-
-  useEffect(() => {
-    lottie.loadAnimation({
-      container: animationContainerOne.current,
-      animationData: lottieFiles.teskiandoTaskCardAnimation,
-      name: 'animationOne',
-    });
-    const animationTwo = lottie.loadAnimation({
-      container: animationContainerTwo.current,
-      animationData: lottieFiles.teskiandoTaskCardAnimation,
-    });
-    animationTwo.setDirection('-1');
-    lottie.loadAnimation({
-      container: animationContainerThree.current,
-      animationData: lottieFiles.teskiandoTaskCardAnimation,
-    });
-    const animationFour = lottie.loadAnimation({
-      container: animationContainerFour.current,
-      animationData: lottieFiles.teskiandoTaskCardAnimation,
-    });
-    animationFour.setDirection('-1');
-    lottie.loadAnimation({
-      container: animationContainerFive.current,
-      animationData: lottieFiles.teskiandoTaskCardAnimation,
-    });
-  }, []);
-
+function TaskCardAnimation({ speed }) {
   return (
-    <MainContainer>
-      <PageGlobalStyle />
-      <AnimationContainer
-        ref={ animationContainerOne }
-      />
-      <AnimationContainer
-        ref={ animationContainerTwo }
-        opacity="80%"
-      />
-      <AnimationContainer
-        ref={ animationContainerThree }
-        opacity="50%"
-      />
-      <AnimationContainer
-        ref={ animationContainerFour }
-        opacity="30%"
-      />
-      <AnimationContainer
-        ref={ animationContainerFive }
-        opacity="15%"
-      />
-    </MainContainer>
+    <CardContainer speed={ speed }>
+      <span>{ dayjs().format('DD/MM/YY HH:mm') }</span>
+      <span>Em progresso</span>
+    </CardContainer>
   );
 }
+
+TaskCardAnimation.defaultProps = {
+  speed: '2s',
+};
+
+TaskCardAnimation.propTypes = {
+  speed: string,
+};
 
 export default TaskCardAnimation;
--- src/components/TaskCardAnimation/style.js
+++ src/components/TaskCardAnimation/style.js
@@ -1,25 +1,69 @@
-import styled, { createGlobalStyle } from 'styled-components';
+import styled from 'styled-components';
 
-export default createGlobalStyle`
-
-  body, main {
-    overflow: hidden;
-  }
-`;
-
-export const MainContainer = styled.div`
-  align-items: center;
+export default styled.div`
+  background-color: #f6f6f6;
+  border-left: 2px;
+  border-radius: 5px;
+  border-right: 2px;
+  color: rgba(0, 0, 0, 0);
   display: flex;
-  flex-flow: column nowrap;
-  height: 100%;
-  max-height: 540px;
-  overflow: hidden;
-`;
+  flex-direction: column;
+  height: 75px;
+  margin-bottom: 5px;
+  margin-top: 2px;
+  position: relative;
+  width: 100%;
 
-export const AnimationContainer = styled.div`
-  height: 23.438vw;
-  max-height: 105.47px;
-  max-width: 450px;
-  opacity: ${({ opacity }) => opacity || '100%'};
-  width: 100vw;
+  span:nth-child( 1 ) {
+    border-bottom-right-radius: 5px;
+    padding-left: 10px;
+    padding-right: 10px;
+    position: absolute;
+  }
+
+  span:nth-child( 2 ) {
+    border-bottom-left-radius: 5px;
+    border-bottom-right-radius: 5px;
+    padding-left: 15px;
+    padding-right: 15px;
+    position: absolute;
+    right: 28px;
+  }
+
+  --gradientFirstColor: #f6f6f6;
+
+  --gradientSecondColor: lightgray;
+
+  --borderGradient: 270deg , var(--gradientFirstColor) , var(--gradientSecondColor);
+
+  span:nth-child( 2 ), span:nth-child( 1 ) {
+    animation: gradientMoviment ${({ speed }) => speed || '2s'} ease infinite;
+    background: linear-gradient(var(--borderGradient));
+    background-size: 400% 400%;
+  }
+
+  --borderWidth: 2px;
+
+  :after {
+    animation: gradientMoviment ${({ speed }) => speed || '2s'} ease infinite;
+    background: linear-gradient(var(--borderGradient));
+    background-size: 300% 300%;
+    border-radius: 5px;
+    content: '';
+    height: calc(100% + var(--borderWidth) * 2);
+    left: calc(-1 * var(--borderWidth));
+    position: absolute;
+    top: calc(-1 * var(--borderWidth));
+    width: calc(100% + var(--borderWidth) * 2);
+    z-index: -1;
+  }
+
+  @keyframes gradientMoviment {
+
+    0% { background-position: 0% 50%; }
+
+    50% { background-position: 100% 50%; }
+
+    100% { background-position: 0% 50%; }
+  }
 `;
