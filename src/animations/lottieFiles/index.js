646d6075495f99aabf9ea6a8f8f70c73ce9932a0
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-03-12 17:43:05 -0300

    feat: animation of aside task cards

--- src/animations/lottieFiles/index.js
+++ src/animations/lottieFiles/index.js
@@ -1,7 +1,10 @@
 import loading from './loading';
+import asideTaskCardsRot from './aside-task-cards-rot.json';
 
 export default {
   loading,
+  asideTaskCardsRot,
 };
 
 export { default as loading } from './loading';
+export { default as asideTaskCardsRot } from './aside-task-cards-rot.json';
