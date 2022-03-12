b519f95a482a877af2b149751ad894c864097d83
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-03-12 17:49:58 -0300

    feat: component `LottieWrapper` for the lottie animations

--- src/animations/components/index.js
+++ src/animations/components/index.js
@@ -1,10 +1,13 @@
 import loading from './loading';
 import TaskCard from './TaskCard';
+import LottieWrapper from './LottieWrapper';
 
 export default {
   loading,
   TaskCard,
+  LottieWrapper,
 };
 
 export { default as loading } from './loading';
 export { default as TaskCard } from './TaskCard';
+export { default as LottieWrapper } from './LottieWrapper';
