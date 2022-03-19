3f59a76bbe2760ef49fee7dfd31c8b86f2799181
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-03-19 17:06:07 -0300

    refactor: add `speedMultiplier` prop to the `TaskList` animation component

--- src/animations/components/loading/TaskList/index.jsx
+++ src/animations/components/loading/TaskList/index.jsx
@@ -1,37 +1,47 @@
+/* eslint-disable no-magic-numbers */
 import React from 'react';
+import { number } from 'prop-types';
 
 import TaskCardAnimation from 'animations/components/TaskCard';
 
 import AnimationContainer, { AnimationsContainer } from './style';
 
-function LoadingTaskList() {
+function TaskList({ speedMultiplier }) {
   return (
     <AnimationsContainer>
       <AnimationContainer opacity="100%">
-        <TaskCardAnimation speed="2s" />
+        <TaskCardAnimation speed={ `${speedMultiplier * 2}s` } />
       </AnimationContainer>
 
       <AnimationContainer opacity="90%">
-        <TaskCardAnimation speed="2.2s" />
+        <TaskCardAnimation speed={ `${speedMultiplier * 2.2}s` } />
       </AnimationContainer>
 
       <AnimationContainer opacity="60%">
-        <TaskCardAnimation speed="2.4s" />
+        <TaskCardAnimation speed={ `${speedMultiplier * 2.4}s` } />
       </AnimationContainer>
 
       <AnimationContainer opacity="40%">
-        <TaskCardAnimation speed="2.6s" />
+        <TaskCardAnimation speed={ `${speedMultiplier * 2.6}s` } />
       </AnimationContainer>
 
       <AnimationContainer opacity="20%">
-        <TaskCardAnimation speed="2.8s" />
+        <TaskCardAnimation speed={ `${speedMultiplier * 2.8}s` } />
       </AnimationContainer>
 
       <AnimationContainer opacity="10%">
-        <TaskCardAnimation speed="3s" />
+        <TaskCardAnimation speed={ `${speedMultiplier * 3}s` } />
       </AnimationContainer>
     </AnimationsContainer>
   );
 }
 
-export default LoadingTaskList;
+TaskList.defaultProps = {
+  speedMultiplier: 1,
+};
+
+TaskList.propTypes = {
+  speedMultiplier: number,
+};
+
+export default TaskList;
